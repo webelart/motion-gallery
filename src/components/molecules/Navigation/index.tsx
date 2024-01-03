89812fd1f823b5d4447c66272e77b7909248ef6c
Elena Litvinova <ivanovaev90@gmail.com>
2024-01-03 12:47:55 +0000

    Motion gallery navigation was added

--- src/components/molecules/Navigation/index.tsx
+++ src/components/molecules/Navigation/index.tsx
@@ -1,11 +1,29 @@
+import cl from 'classnames';
 import { ComponentProps } from '@components/types';
+
+import styles from './index.module.scss'
+
+const navigation = [
+  { id: 1, url: '/', title: 'Home' },
+  { id: 2, url: '/', title: 'Stories' },
+  { id: 3, url: '/', title: 'Works' },
+  { id: 4, url: '/', title: 'Pages' },
+  { id: 5, url: '/', title: 'Contact' },
+  { id: 6, url: '/', title: 'Shop' }
+]
 
 export default function Navigation({
   className
 }: ComponentProps) {
   return (
-    <div>
-      Navigation
-    </div>
+    <ul className={cl(className, styles.navigation)}>
+      {navigation.map(({id, url, title}) => (
+        <li key={id} className={styles.navigationItem}>
+          <a href={url} className={styles.navigationLink}>
+            {title}
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 }
